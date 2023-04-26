7ac72bc07b5ddca2ce855bf4c15b66ad8ad536bb
Adan Saavedra <adansaavedraa@icloud.com>
2023-04-26 00:37:01 -0500

    mutable

---
+++ server/16-mutable.js
@@ -0,0 +1,36 @@
+const products = [
+    { title: 'Pizza', price: 121, id: '🍕' },
+    { title: 'Burger', price: 121, id: '🍔' },
+    { title: 'Hot cakes', price: 121, id: '🥞' },
+];
+
+const myProducts = [];
+
+console.log("products", products);
+console.log("myProducts", myProducts);
+console.log("-".repeat(10));
+
+const productIndex = products.findIndex(item => item.id==='🍔')
+if(productIndex !== -1){
+    myProducts.push(products[productIndex])
+    products.splice(productIndex,1)
+}
+console.log("products", products);
+console.log("myProducts", myProducts);
+console.log("-".repeat(10));
+
+// update
+
+const productsV2 = [
+    { title: 'Pizza', price: 121, id: '🍕' },
+    { title: 'Burger', price: 121, id: '🍔' },
+    { title: 'Hot cakes', price: 121, id: '🥞' },
+];
+
+const update = {
+    id: '🥞',
+    changes: {
+        price: 200,
+        description: 'delicioso'
+    }
+}
